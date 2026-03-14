f2d9e4d86d5a3c26cfb1e52bce6bbb8940ca6b6e
altasebchernet <altasebchernet01@gmail.com>
2026-03-14 14:37:37 +0300

    file

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ module.exports = {
 "use strict";
 
 var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'home.html',
+            controller: 'HomeController'
+        })  
+        .when('/about', {
+            templateUrl: 'about.html',
+            controller: 'AboutController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+});
+
+app.controller('HomeController', function ($scope) {
+    $scope.message = 'Welcome to the Home Page!';
+});
